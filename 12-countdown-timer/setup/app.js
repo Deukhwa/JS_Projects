181c0bb5f5a943cfbbfb4b1fb8f8bb19e14e2372
Deukhwa <deukhwajeong@gmail.com>
2022-02-24 08:16:19 -0500

    Finish project12 - countdown

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -26,8 +26,14 @@ const giveaway = document.querySelector('.giveaway')
 const deadline = document.querySelector('.deadline')
 const items = document.querySelectorAll('.deadline-format h4')
 
-let futureDate = new Date(2022, 1, 24, 11, 30, 0)
+let tempDate = new Date()
+let tempYear = tempDate.getFullYear()
+let tempMonth = tempDate.getMonth()
+let tempDay = tempDate.getDate()
 
+// let futureDate = new Date(2022, 1, 24, 8, 13, 0)
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0)
 const year = futureDate.getFullYear()
 const hours = futureDate.getHours()
 const minutes = futureDate.getMinutes()
@@ -75,8 +81,14 @@ const getRemainingTime = () => {
   items.forEach((item, index) => {
     item.innerHTML = format(values[index])
   })
+
+  if (t < 0) {
+    clearInterval(countdown)
+    deadline.innerHTML = `<h4 class="expired"> Sorry, this giveaway has expired</h4>`
+  }
 }
 
 //count down
 
 let countdown = setInterval(getRemainingTime, 1000)
+// getRemainingTime()
