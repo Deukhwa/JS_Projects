8cdb69434afe23ce6b64d968d15b4ec3b768e89b
Deukhwa <deukhwajeong@gmail.com>
2022-03-02 21:59:15 -0500

    Add delete and edit buttons

--- 14-grocery-bud/setup/app.js
+++ 14-grocery-bud/setup/app.js
@@ -33,15 +33,59 @@ form.addEventListener('submit', (e) => {
       </button>
     </div>
     `
+    const deleteBtn = element.querySelector('.delete-btn')
+    const editBtn = element.querySelector('.edit-btn')
+
+    deleteBtn.addEventListener('click', (e) => {
+      const item = e.currentTarget.parentElement.parentElement
+      const id = item.dataset.id
+
+      list.removeChild(item)
+      if (list.children.length === 0) {
+        container.classList.remove('show-container')
+      }
+      displayAlert('item removed', 'danger')
+      setBackToDefault()
+      removeFromLocalStorage(id)
+    })
+
+    editBtn.addEventListener('click', (e) => {
+      const element = e.currentTarget.parentElement.parentElement
+      // set edit item
+      editElement = e.currentTarget.parentElement.previousElementSibling
+      // set from value
+      grocery.value = editElement.innerHTML
+      editFlag = true
+      editID = element.dataset.id
+      submitBtn.textContent = 'edit'
+    })
+
     list.appendChild(element)
     displayAlert('item added to the list', 'success')
     container.classList.add('show-container')
     addToLocalStorage(id, value)
     setBackToDefault()
   } else if (value && editFlag) {
+    editElement.innerHTML = value
+    displayAlert('value changed', 'success')
+    editLocalStorage(editID, value)
+    setBackToDefault()
+    addToLocalStorage(id, value)
   } else {
     displayAlert('empty value', 'danger')
   }
+})
+
+clearBtn.addEventListener('click', () => {
+  const items = document.querySelectorAll('.grocery-item')
+  if (items.length > 0) {
+    items.forEach((item) => {
+      list.removeChild(item)
+    })
+  }
+  container.classList.remove('show-container')
+  displayAlert('empty list', 'danger')
+  setBackToDefault()
 })
 // ****** FUNCTIONS **********
 const displayAlert = (text, action) => {
@@ -54,6 +98,15 @@ const displayAlert = (text, action) => {
   }, 1000)
 }
 
+const setBackToDefault = () => {
+  grocery.value = ''
+  editFlag = false
+  editID = ''
+  submitBtn.textContent = 'submit'
+}
+
 // ****** LOCAL STORAGE **********
 const addToLocalStorage = (id, value) => {}
+const removeFromLocalStorage = (id) => {}
+const editLocalStorage = (id, value) => {}
 // ****** SETUP ITEMS **********
